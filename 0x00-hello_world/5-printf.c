bc7738b76cc4a266e01e6740968d97a9be54aab1
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-09 10:43:05 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-                                                           int main(void)                                             {                                                                  printf ("/"Programming is like building a multilingual puzzle");					
-	return(0)
-							   }	     	
+int main(void)                                             {                                                                  printf ("/"with proper grammar, but the outcome is a piece of art");
+	return(0);			                   }	    	
