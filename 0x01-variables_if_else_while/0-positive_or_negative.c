0919052eb2768de30f1caddd42e8a657f5c13e6d
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-12 08:35:01 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
 /*
  * new - Entery point
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int n;
@@ -14,15 +16,16 @@ int main(void)
 
 	if (n>0)
 	{
-		printf("%i is positive \n",n);
+		printf("%d is positive \n",n);
 	}
 	else if (n == 0)
 	{
-		printf("%i is zero \n",n);
+		printf("%d is zero \n",n);
 	}
 	else if (n<0)
 	{
-		printf("%i is negative \n",n);
+		printf("%d is negative \n",n);
 	}
-		return (0);
+
+        	return (0);
 }
