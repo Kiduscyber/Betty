607ffbd861ef10603f505a329e29e56a5d7c5224
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-13 12:23:39 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0 .
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putcher('u');
+	_putcher('t');
+	_putcher('c');
+	_putcher('h');
+	_putcher('e');
+	_putcher('r');
+	_putcher('\n');
+
+	return (0);
+}
