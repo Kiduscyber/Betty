7c22c231c840efa04dea993b1230209e61616f64
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-15 10:53:21 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,17 +10,8 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incoming :(\n");
-
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	}
-
-	printf("infinite loop avoided! \\o/\n");
+	positive_or_negative(i);
 
 	return (0);
-}
 
