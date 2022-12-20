6f6312dbb32766ad34e4046c076560843f698e8f
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-20 09:20:25 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed by src,
+ * including the terminating null byte,
+ * to the buffer pointed to by dest,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		*(dest + coucou) = *(src + count);
+		if (*(srv + count) == '\0')
+			break;
+	}
+	return (dest);
+}
