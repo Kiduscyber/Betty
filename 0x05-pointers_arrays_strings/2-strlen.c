3865b217c190608cd8c9d105e8780a18f71721c7
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-20 10:34:00 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
  * _strlen - returns the length of string
- * s@: input stringeturns
+ * @s: input stringeturns
  * Return: lengeth of a string
  */
 
 int _strlen(char *s)
 {
 	int count = 0;
-	
+
 	while (*(s + count) != '\0')
 		count++;
 	return (count);
