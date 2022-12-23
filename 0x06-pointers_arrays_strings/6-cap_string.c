196936eb8bca374a2dd8eefaa1422784cd1308ed
kiduscyber <kidus.tesfaye555@gmail.com>
2022-12-23 08:36:01 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -27,7 +27,7 @@ char *cap_string(char *str)
 		str[index] == '(' ||
 		str[index] == ')' ||
 		str[index] == '{' ||
-		tr[index] == '}' ||
+		str[index] == '}' ||
 		index == 0)
 			str[index] -= 32;
 		index++;
